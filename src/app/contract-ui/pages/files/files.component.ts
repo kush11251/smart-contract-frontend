fe46c1de569582264ea0ed8e34f4b116f5cdd2cc
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-03-03 21:50:37 +0530

    File upload code updated

--- src/app/contract-ui/pages/files/files.component.ts
+++ src/app/contract-ui/pages/files/files.component.ts
@@ -1,6 +1,8 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, Renderer2 } from '@angular/core';
 import { ApiCallService } from '../../services/api-call.service';
 import { Router } from '@angular/router';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-files',
@@ -10,41 +12,115 @@ import { Router } from '@angular/router';
 export class FilesComponent implements OnInit {
   userInput: string = '';
 
+  private pinataApiKey = '0b4b77f453c17f95e661';
+  private pinataSecretApiKey = '5475c5d3e35cd289ad746ec948406d48107bdf61d3dade059225b9dfe61f0115';
+  private pinataUrl = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
+  private pinataListUrl = 'https://api.pinata.cloud/data/pinList';
+
   constructor(
     private apiService: ApiCallService,
     private router: Router,
-    private ngZone: NgZone
+    private ngZone: NgZone,
+    private renderer: Renderer2
   ) {}
 
-  ngOnInit(): void {
-    // this.apiService.getGetRequest("/contract-data").subscribe((res) => {
-    //   console.log(res)
-    // })
-  }
+  ngOnInit(): void {}
 
   logInput(): void {
     console.log('User Input:', this.userInput);
 
-    this.apiService
-      .getGetRequest('/getLoanHTML/' + this.userInput)
-      .subscribe((res: any) => {
-        console.log(res);
+    this.apiService.getGetRequest('/getLoanHTML/' + this.userInput).subscribe(async (res: any) => {
+      console.log(res);
+      if (res) {
+        console.log('Res File: ', res?.file);
+        await this.generatePDF(res?.file, `loan_${this.userInput}.pdf`);
+      }
+    });
+  }
 
-        if (res) {
-          this.ngZone.run(() => {
-            this.router.navigate(['/view-file'], { state: { loanHtml: res?.file } });
-          });
+  checkFileOnPinata(pdfBlob: Blob, fileName: string) {
+    const headers = {
+      'pinata_api_key': this.pinataApiKey,
+      'pinata_secret_api_key': this.pinataSecretApiKey,
+    };
+
+    fetch(`${this.pinataListUrl}?metadata[name]=${fileName}`, {
+      method: 'GET',
+      headers: headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.count > 0) {
+          const existingFileHash = data.rows[0].ipfs_pin_hash;
+          console.log('File already exists on IPFS:', existingFileHash);
+          this.viewFile(existingFileHash);
+        } else {
+          this.uploadFileToPinata(pdfBlob, fileName);
         }
+      })
+      .catch((error) => {
+        console.error('Error checking file on Pinata:', error);
+        this.uploadFileToPinata(pdfBlob, fileName);
       });
   }
 
-  downloadFile(url: string) {
-    const link = document.createElement('a');
-    link.href = url;
-    link.target = '_blank'; // Open in a new tab (optional)
-    link.download = url.split('/').pop() || 'downloaded-file'; // Extract filename from URL
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  uploadFileToPinata(pdfBlob: Blob, fileName: string) {
+    const formData: FormData = new FormData();
+    formData.append('file', pdfBlob, fileName);
+
+    fetch(this.pinataUrl, {
+      method: 'POST',
+      headers: {
+        'pinata_api_key': this.pinataApiKey,
+        'pinata_secret_api_key': this.pinataSecretApiKey,
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Pinata Response:', data);
+        if (data.IpfsHash) {
+          console.log('File uploaded to IPFS:', data.IpfsHash);
+          alert(`File uploaded successfully!\nIPFS Hash: ${data.IpfsHash}`);
+          this.viewFile(data.IpfsHash);
+        }
+      })
+      .catch((error) => console.error('Error uploading to Pinata:', error));
+  }
+
+  viewFile(ipfsHash: string) {
+    const fileUrl = `https://gateway.pinata.cloud/ipfs/${ipfsHash}`;
+    window.open(fileUrl, '_blank');
+  }
+
+  generatePDF(htmlContent: string, fileName: string): void {
+    const container = document.createElement('div');
+    container.innerHTML = htmlContent;
+    document.body.appendChild(container);
+
+    html2canvas(container).then(async canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const imgWidth = 210;
+      const pageHeight = 297;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 0;
+
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft > 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      const blob = pdf.output('blob');
+      this.checkFileOnPinata(await blob, fileName);
+
+      document.body.removeChild(container);
+    });
   }
 }
